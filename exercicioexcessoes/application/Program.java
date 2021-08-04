271ef0a37bdc5a52184d27392d27960f933c8e1b
viniarnhold <viniciusarnh@gmail.com>
2021-08-04 14:12:36 -0300

    Exercício de fixação sobre tratamento e criação de exceções personalizadas

---
+++ exercicioexcessoes/application/Program.java
@@ -0,0 +1,42 @@
+package exercicioexcessoes.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import exercicioexcessoes.domain.Account;
+import exercicioexcessoes.domain.exceptions.DomainException;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+            System.out.println("Enter account data:");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder: ");
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.println();
+            System.out.print("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+            account.withdraw(amount);
+            System.out.println(account.toString());
+        }
+        catch (DomainException e){
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+
+
+
+        sc.close();
+    }
+}
